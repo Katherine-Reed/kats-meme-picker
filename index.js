ee74d7a9d0811a546025bda3013870db8e78858c
Kat Grace <katherine.grace.reed@gmail.com>
2024-11-08 18:50:01 +0000

    Get the ID

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
-function getEmotionsArray(cats) {
-    const emotionsArray = []
+emotionRadios.addEventListener('change', function(e){
+    console.log(e.target.id)
+})
 
-    for (let cat of cats) {
-        for (let emotion of cat.emotionTags) {
-            if (!emotionsArray.includes(emotion)) {
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
                 emotionsArray.push(emotion)
             }
         }
@@ -15,10 +18,11 @@ function getEmotionsArray(cats) {
     return emotionsArray
 }
 
-function renderEmotionsRadios(cats) {
+function renderEmotionsRadios(cats){
+        
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions) {
+    for (let emotion of emotions){
         radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
